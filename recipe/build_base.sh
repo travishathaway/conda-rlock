9763608164a921da61b155ce45ccad781f9ec66c
Travis Hathaway <travis.j.hathaway@gmail.com>
2025-03-10 16:09:08 -0500

    updating Cargo.toml

--- recipe/build_base.sh
+++ recipe/build_base.sh
@@ -7,9 +7,6 @@ export CARGO_PROFILE_RELEASE_LTO=fat
 
 export OPENSSL_DIR=$PREFIX
 
-# Use native-tls on conda-forge
-# export MATURIN_PEP517_ARGS="--no-default-features --features=native-tls"
-
 # Run the maturin build via pip which works for direct and
 # cross-compiled builds.
 $PYTHON -m pip install . -vv
